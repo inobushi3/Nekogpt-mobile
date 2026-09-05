c18a276a82f43063ed56d43450d0b47b67126edf
Ruan Lucas <ruanlucasns97@gmail.com>
2026-09-05 19:24:45 -0300

    Show three styled dialogue entries

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { installDialogueEffects } from './dialogue-effects';
+import { installDialogueWindow } from './dialogue-window';
 import './styles.css';
 import './dialogue-reference.css';
 import './dialogue-effects.css';
+import './dialogue-message-layout.css';
 
 if ('serviceWorker' in navigator && import.meta.env.PROD) {
   window.addEventListener('load', () => {
     void navigator.serviceWorker.register('/sw.js');
   });
 }
+
+installDialogueWindow();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
