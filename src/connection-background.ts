e627e084003b08147db47f264ac8eb0ecccbd573
Ruan Lucas <ruanlucasns97@gmail.com>
2026-09-11 14:21:40 -0300

    Assemble optimized connection background

---
+++ src/connection-background.ts
@@ -0,0 +1,6 @@
+import part1 from './assets/connection-bg-part-1';
+import part2 from './assets/connection-bg-part-2';
+import part3 from './assets/connection-bg-part-3';
+import part4 from './assets/connection-bg-part-4';
+
+export const CONNECTION_BACKGROUND_SRC = `data:image/webp;base64,${part1}${part2}${part3}${part4}`;
